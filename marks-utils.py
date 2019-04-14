98b27135859874ead005bb48153534813eb13c5f
markschmidt42 <mschmidt@bizstream.com>
2019-04-14 08:17:14 -0400

    Added Google Drive Functions

---
+++ marks-utils.py
@@ -0,0 +1,21 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+
+# Authenticate and create the PyDrive client.
+def auth_google_drive():
+  auth.authenticate_user()
+  gauth = GoogleAuth()
+  gauth.credentials = GoogleCredentials.get_application_default()
+  drive = GoogleDrive(gauth)
+  return drive
+
+def get_data_from_google_drive(file_id, save_as):
+  drive = auth_google_drive()
+  
+  downloaded = drive.CreateFile({'id':file_id}) 
+  downloaded.GetContentFile(save_as)  
+  data_csv = pd.read_csv(save_as)
+
+  return data_csv
